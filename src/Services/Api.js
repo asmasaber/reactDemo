e119d837bbc9907a57dea6a1630a24f445d19859
asmasaber <asmaa.saber93@gmail.com>
2019-08-21 12:32:11 +0200

    update api service to export on object

--- src/Services/Api.js
+++ src/Services/Api.js
@@ -6,24 +6,22 @@ const Api = apisauce.create({
   headers: { Accept: "application/json; charset=UTF-8" },
 });
  
-const posts = {
-  getAll: () => Api.get("/posts"),
-  get: id => Api.get(`/posts/${id}`),
-  getUserPosts: id => Api.get(`/users/${id}/posts`),
-  create: item => Api.post("/posts", item),
-  update: data => Api.put(`/posts/${data.id}`, data),
-  delete: id => Api.delete(`/posts/${id}`),
+export default { 
+  posts: {
+    getAll: () => Api.get("/posts"),
+    get: id => Api.get(`/posts/${id}`),
+    getUserPosts: id => Api.get(`/users/${id}/posts`),
+    create: item => Api.post("/posts", item),
+    update: data => Api.put(`/posts/${data.id}`, data),
+    delete: id => Api.delete(`/posts/${id}`),
+  },
+  comments: {
+    get: id => Api.get(`/comments/${id}`),
+    getPostComments: id => Api.get(`/posts/${id}/comments`),
+    create: item => Api.post(`/posts/${item.postId}/comments`, item),
+    update: data => Api.put(`comments/${data.id}`, data),
+    delete: id => Api.delete(`comments/${id}`),
+  }
 };
 
-const comments = {
-  get: id => Api.get(`/comments/${id}`),
-  getPostComments: id => Api.get(`/posts/${id}/comments`),
-  create: item => Api.post(`/posts/${item.postId}/comments`, item),
-  update: data => Api.put(`comments/${data.id}`, data),
-  delete: id => Api.delete(`comments/${id}`),
-};
 
-export default {
-  posts,
-  comments
-};
